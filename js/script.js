991e8687413593ab43c3841d7fc7fe5565b93cd3
Iryna127 <irusia_1111@ukr.net>
2022-12-15 21:51:50 +0200

    delete count

--- js/script.js
+++ js/script.js
@@ -1,47 +1,41 @@
 //Первое задание
 
- let date = prompt("Введите ваше имя:");
-let count = 1;
+let date = prompt("Введите ваше имя:");
 for (let i = 1; i <= 4; i++) {
-  if (count == 3) {
+  if (i == 3) {
     console.log(`Happy birthday, dear ${date}`);
-    count += 1;
-  } else if(count == 1){
+  } else if (i == 1) {
     console.log(`Happy birthday to you`);
-    count += 1;
-  } else if (count == 2){
+  } else if (i == 2) {
     console.log(`Happy birthday to you`);
-    count += 1;
   } else {
     console.log(`Happy birthday to you`);
-    count += 1;
   }
-} 
+}
 
 //Второе задание
 
-simvol = '';
-let newDate = +prompt('Введите число:');
+simvol = "";
+let newDate = +prompt("Введите число:");
 for (let i = 1; i <= newDate; i++) {
-  simvol += '.#';
+  simvol += ".#";
 }
 console.log(simvol);
 
 // Третье задание
-let number = +prompt('Введите число');
+let number = +prompt("Введите число");
 if (Boolean(number) == true) {
 } else {
-   number = +prompt('Invalid. You should enter a number", "Try again');
+  number = +prompt('Invalid. You should enter a number", "Try again');
 }
 let numberOdd = 0;
 for (let i = 0; i <= number; i++) {
   if (i % 2 === 1) {
-    numberOdd += i;    
-  } 
+    numberOdd += i;
+  }
 }
 let result = numberOdd;
 console.log(`Сума нечетных чисел = ${result}`);
-
 
 // Четвертое задание
 let interation = 1;
@@ -69,16 +63,10 @@ for (let i = 1; i < 50; i++) {
   } else {
     console.log(i);
   }
-} 
+}
 
-// Шестое задание
+// Six task
 let dateNew = new Date();
 console.log(dateNew.toUTCString());
 console.log(dateNew.toDateString());
 console.log(dateNew);
-
-
-
-
-
-
